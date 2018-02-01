aa035380a2cec238a62135d61456e9771d40dbcc
Jaehwan <dialektike@gmail.com>
2018-02-01 17:59:48 +0900

    내용추가

--- clint_test.py
+++ clint_test.py
@@ -8,11 +8,16 @@ import os
 
 sys.path.insert(0, os.path.abspath('..'))
 
-from clint.textui import colored
+from clint.arguments import Args 
+from clint.textui import puts, colored, indent
 
-text = 'THIS TEXT IS COLORED %s!'
+args = Args()
 
-if __name__ == '__main__':
+with indent(4, quote='>>>'):
+    puts(colored.red('Aruments passed in: ') + str(args.all))
+    puts(colored.red('Flags detected: ') + str(args.flags))
+    puts(colored.red('Files detected: ') + str(args.files))
+    puts(colored.red('NOT Files detected: ') + str(args.not_files))
+    puts(colored.red('Grouped Arguments: ') + str(dict(args.grouped)))
 
-    for color in colored.COLORS:
-		print(getattr(colored, color)(text % color.upper())
+print()
