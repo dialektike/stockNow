5abe5935ae909509bdc33c044c27357d65acbb5b
Jaehwan <dialektike@gmail.com>
2018-02-06 17:59:34 +0900

    폰트 추가

--- sphd.py
+++ sphd.py
@@ -1,10 +1,11 @@
 import scrollphathd as sphd
+from scrollphathd.fonts import font5x7
 import time
 
 sphd.set_brightness(0.5)
 sphd.rotate(degrees= 180)
     
-sphd.write_string('Shiver me timbers!')
+sphd.write_string('Shiver me timbers!', font=font5x7)
 
 while True:
     sphd.show()
